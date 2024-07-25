5ffef4f0b306b655c9ba552ee1f2a91835491208
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-07-25 08:40:17 -0400

    feat: try to fix unsecure authentication

--- src/masqr.ts
+++ src/masqr.ts
@@ -7,8 +7,18 @@ type MasqrConfig = {
   whitelist: string[];
 }
 
-export async function masqrCheck(config: MasqrConfig, htmlFile: string): Promise<Function> {
+export async function masqrCheck(config: MasqrConfig, htmlFile: string, idFile: string): Promise<Function> {
   let loadedHTMLFile = fs.readFileSync(htmlFile, "utf8");
+  let storedIDs = [];
+  if (fs.existsSync(idFile)) {
+    const data = fs.readFileSync(idFile, "utf8");
+    storedIDs = JSON.parse(data);
+  }
+  // Save the data to a file incase your server kills itself or something
+  setInterval(() => {
+    const data = JSON.stringify(storedIDs);
+    fs.writeFile(idFile, data, (res) => {console.log('Saved Data')});
+  }, 5000);
   return async (req: Request, res: Response, next: NextFunction) => {
     if (req.headers.host && config.whitelist.includes(req.headers.host)) {
       next();
@@ -16,8 +26,14 @@ export async function masqrCheck(config: MasqrConfig, htmlFile: string): Promise
     }
     const authheader = req.headers.authorization;
     if (req.cookies["authcheck"]) {
-      next();
-      return;
+      const isVerified = storedIDs.includes(req.cookies["authcheck"]);
+      if (isVerified) {
+        next();
+        return;
+      } else {
+        res.setHeader("Content-Type", "text/html"); 
+        return res.status(401).send(failureFile);
+      }
     }
     if (!authheader) {
       res.setHeader("WWW-Authenticate", "Basic");
@@ -33,7 +49,14 @@ export async function masqrCheck(config: MasqrConfig, htmlFile: string): Promise
 
     if (licenseCheck === "License valid") {
       // Authenticated, set cookie for a year
-      res.cookie("authcheck", "true", {
+      const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let code = '';
+      for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters[randomIndex];
+      }
+      storedIDs.push(code);
+      res.cookie("authcheck", code, {
         expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000),
       });
       res.send(`<script>window.location.href = window.location.href</script>`); // fun hack to make the browser refresh and remove the auth params from the URL
